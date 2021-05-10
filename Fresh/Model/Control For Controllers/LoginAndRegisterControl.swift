84c1444174eca0b87ba5a7477a6ce61bc8867e9e
Richard Basdeo <basdeorichard@gmail.com>
2021-05-10 12:53:40 -0400

    Basic Setup. - Richard

---
+++ Fresh/Model/Control For Controllers/LoginAndRegisterControl.swift
@@ -0,0 +1,10 @@
+//
+//  LoginAndRegisterControl.swift
+//  Fresh
+//
+//  Created by Richard Basdeo on 5/10/21.
+//
+
+import UIKit
+class LoginAndRegisterControl {
+}
---
+++ Fresh/Model/Styling.swift
@@ -0,0 +1,11 @@
+//
+//  Styling.swift
+//  Fresh
+//
+//  Created by Richard Basdeo on 5/10/21.
+//
+
+import UIKit
+class Styling {
+    
+}
